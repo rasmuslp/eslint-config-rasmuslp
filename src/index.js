e777b86003aa420e4f109efad2f2763054173a06
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2025-09-06 15:17:14 +0200

    feat: Add eslint-config-flat-gitignore

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
+import gitignore from 'eslint-config-flat-gitignore';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
+	gitignore(),
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
