f23b8f6e533f7d0c651c47017760999323a5dabf
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2025-09-06 16:15:33 +0200

    chore: Port some eslint config

--- src/index.js
+++ src/index.js
@@ -7,14 +7,42 @@ import tseslint from 'typescript-eslint';
 
 export default defineConfig(
 	gitignore(),
-	eslint.configs.recommended,
-	tseslint.configs.strictTypeChecked,
-	tseslint.configs.stylisticTypeChecked,
 	{
+		extends: [eslint.configs.recommended],
+		rules: {
+			curly: ['error', 'all'],
+			'sort-imports': [
+				'error',
+				{
+					ignoreDeclarationSort: true,
+				},
+			],
+		},
+	},
+	{
+		extends: [
+			tseslint.configs.strictTypeChecked,
+			tseslint.configs.stylisticTypeChecked,
+		],
 		languageOptions: {
 			parserOptions: {
 				projectService: true,
 			},
+		},
+		rules: {
+			'@typescript-eslint/consistent-type-exports': 'error',
+			'@typescript-eslint/consistent-type-imports': 'error',
+			'@typescript-eslint/method-signature-style': ['error', 'property'],
+			'@typescript-eslint/non-nullable-type-assertion-style': 'error',
+			'@typescript-eslint/require-await': 'off',
+			'@typescript-eslint/restrict-template-expressions': [
+				'error',
+				{
+					allowNumber: true,
+				},
+			],
+			'@typescript-eslint/return-await': ['error', 'in-try-catch'],
+			'@typescript-eslint/switch-exhaustiveness-check': 'error',
 		},
 	},
 );
